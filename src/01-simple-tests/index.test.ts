9cbe9b3e60ec52c8e99668fab60674f80d7446e8
Soso <soso.beriashvili@redberry.ge>
2024-10-15 16:02:24 +0400

    feat: implement simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,74 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 1,
+      b: 2,
+      action: Action.Add,
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toEqual(3);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 10,
+      b: 5,
+      action: Action.Subtract,
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toEqual(5);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 2,
+      b: 5,
+      action: Action.Multiply,
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toEqual(10);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 10,
+      b: 5,
+      action: Action.Divide,
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toEqual(2);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 2,
+      b: 4,
+      action: Action.Exponentiate,
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toEqual(16);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 2,
+      b: 4,
+      action: 'unknown action',
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const dataToCalculate = {
+      a: 2,
+      b: '4',
+      action: Action.Add,
+    };
+
+    expect(simpleCalculator(dataToCalculate)).toBeNull();
   });
 });
